26bd028462d6e707be94a8a4dffac8c7f3390fa0
Jackeline <cariiasdiaz@gmail.com>
2025-01-30 14:28:45 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://sjcd.githun.io",
+  site: "https://sjcd.github.io",
   base: "/",
   trailingSlash: "always",
   integrations: [
